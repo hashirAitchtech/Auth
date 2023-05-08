e38d5f3c601ba1e10fe7b7687467a0e81ec79b82
Muhammad Hashir <hashir.aitchtech@outlook.com>
2023-05-08 11:32:52 +0500

    excluded __v and password field from results

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,7 +62,7 @@ exports.getAllUsers = async (req, res) => {
     try {
         
         // const users = User.find().select('-password').lean();
-        const users = await User.find().select('-password -__v').lean();
+        const users = await User.find().select('-__v').lean();
         res.status(200).json(users)
     } catch (error) {
         res.status(404).json({
@@ -77,22 +77,21 @@ exports.register = async (req, res) => {
     try {
         const { username, password, roles } = req.body;
 
-        // if (!username || !password || !Array.isArray(roles) || !roles.length) {
-        //     return res.status(400).json({
-        //         message: 'All fields are required',
-        //     })
-        // }
-
-        // Hash password
+        if (!username || !password) {
+            return res.status(400).json({
+                message: 'All fields are required',
+            })
+        }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const userObj = {username, password: hashedPassword, roles}
 
         const newUser = await User.create(userObj);
-        res.status(201).json({
-            data: newUser
-        })
+
+        newUser.password = undefined;
+        
+        res.status(201).json(newUser)
 
     } catch (error) {
         res.status(404).json({
@@ -117,7 +116,7 @@ exports.logout = async (req, res) => {
             status: 'success',
         })
     } catch (error) {
-        res.status(201).json({
+        res.status(400).json({
             status: 'failed',
             error: error
         })
--- models/Users.js
+++ models/Users.js
@@ -8,17 +8,29 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [true, 'Password is required']
+        required: [true, 'Password is required'],
+        select: false
     },
     roles: {
         type: [String],
+        enum: {
+            values: ['Employee', 'Manager', 'Owner'],
+            message: 'Invalid Role'
+        },
         default: ["Employee"]
     },
     active: {
         type: Boolean,
         default: true
     }
+}, {
+    toJSON: {
+        transform: function (doc, ret){
+            delete ret.__v;
+        }
+    }
 })
+
 
 const User = mongoose.model('user', userSchema);
 
